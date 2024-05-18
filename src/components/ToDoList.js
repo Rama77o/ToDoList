35b75bf1aa5d7c5339a806d150fb5eb8d2fb678e
Rama77o <133762230+Rama77o@users.noreply.github.com>
2024-05-19 01:31:53 +0300

    Update ToDoList.js

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,27 +1,33 @@
-import React, { useState } from 'react'
-import shortid from 'shortid';
+import React, { useState } from "react";
+import shortid from "shortid";
 
 const ToDoList = (props) => {
-const [text, setText] = useState("");
+  const [text, setText] = useState("");
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     props.onSubmit({
-        id: shortid.generate(),
-        text: text,
-        complete: false
-    })
+      id: shortid.generate(),
+      text: text,
+      complete: false,
+    });
     setText("");
-}
+  };
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-            <input type='text'  value={text} onChange={(e) => setText(e.target.value)}/>
-            <button className='btn btn-[#afeeee]' onClick={handleSubmit}>add todo</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className="btn btn-[#afeeee]" onClick={handleSubmit}>
+          add todo
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ToDoList
+export default ToDoList;
