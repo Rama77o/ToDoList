fbb0d4f49e76fb183b2b814d83a61e75b6d58c33
Rama77o <133762230+Rama77o@users.noreply.github.com>
2024-05-19 01:32:32 +0300

    Update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const ToDo = (props) => {
   return (
-    <div className='todo'>
-        <div style={{textDecoration: props.toDo.complete ? "line-through" : ""}} onClick={props.toggleComplete}>{props.toDo.text}</div>
-        <button className='btn btn-[#6610f2]' onClick={props.onDelete}>x</button>
+    <div className="todo">
+      <div
+        style={{
+          textDecoration: props.toDo.complete ? "line-through" : "",
+          fontSize: "30px",
+          paddingRight: "100px",
+        }}
+        onClick={props.toggleComplete}
+      >
+        {props.toDo.text}
+      </div>
+      <button
+        className="btn btn-[#6610f2]"
+        onClick={props.onDelete}
+        style={{ borderRadius: " 50%", height: "50px", width: "50px" }}
+      >
+        x
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default ToDo;
